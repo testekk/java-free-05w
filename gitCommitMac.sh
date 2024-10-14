94f3b9836821c620b29d9784b98d73a08870cf4f
Duy Bui Gia <duy.bg@shb.com.vn>
2024-10-14 11:54:49 +0700

    Vincent: Add Git command

--- gitCommitMac.sh
+++ gitCommitMac.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Thực hiện cấp quyền thực thi cho file này bằng lệnh chmod +x gitCommitMac.sh
 # Pull new changes from the remote repository
-git pull origin feature/duybg
+git pull origin feature/20241014
 
 # Stage all changes
 # git add [Your file path] ví dụ git add src/VincentLoopExercise.java
@@ -12,4 +12,4 @@ git add gitCommitMac.sh
 git commit -m "Vincent: Add Git Commit Mac"
 
 # Push changes to the remote repository
-git push origin feature/duybg
+git push origin feature/20241014
--- gitCommitWin.bat
+++ gitCommitWin.bat
@@ -2,7 +2,7 @@
 REM Bạn hãy comment toàn bộ file của bạn trước khi commit code nhé ^^
 
 REM Pull new changes from the remote repository
-git pull origin feature/auteam
+git pull origin feature/20241014
 
 REM Stage all changes
 git add src/[Tên file cần commit]
@@ -12,6 +12,6 @@ REM git commit -m "[Your name]: [Your message]"
 git commit -m "Vincent: Bài tập buổi 02"
 
 REM Push changes to the remote repository
-git push origin feature/auteam
+git push origin feature/20241014
 
 pause
