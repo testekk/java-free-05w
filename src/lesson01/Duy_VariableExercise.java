31852fa1fbaccb8216c788122767d933cf6e0d0c
Duy Bui Gia <duy.bg@shb.com.vn>
2024-10-14 20:26:56 +0700

    Vincent: Add Bài thực hành 01

---
+++ src/lesson01/Duy_VariableExercise.java
@@ -0,0 +1,32 @@
+package lesson01;
+
+/**
+ * Các bạn sẽ phát triển code của các bạn ở đây, lưu ý mỗi người tạo 01 file và đặt tên theo rule
+ * 1. Copy file này, và change name của file theo cú pháp sau: YourName_VariableExercise.java
+ *  Ví dụ: Nguyễn Văn An -> NguyenVanAn_VariableExercise.java
+ * 2. Truy cập file gitCommitMac.sh hoặc gitCommitWin.bat (Lựa chọn theo HĐH bạn đang sử dụng) và thêm thông tin commit của bạn.
+ *   2.1 Tại dòng 8: Thay đường dẫn file của bạn tại [Your file Path] (Ví dụ: src/lesson01/NguyenVanAn_VariableExercise.java)
+ *   2.2 Truy cập terminal có sử dụng IntelliJ IDEA: Vào View -> Tool Windows -> Terminal và thực thi dòng lệnh sau:
+ *    - Trên Mac: sh gitCommitMac.sh
+ *    - Trên Windows: gitCommitWin.bat
+ *    2.3 Nếu gặp lỗi về quyền truy cập thì bạn cần cấp quyền cho file .sh hoặc .bat đó, mở terminal và thực thi lệnh sau:
+ *    - Trên Mac: chmod +x gitCommitMac.sh
+ *    - Trên Windows: Không cần thực thi lệnh này
+ *  3. Sau khi thực thi xong, bạn sẽ thấy thông báo "Git commit successfully" trên terminal
+ */
+public class Duy_VariableExercise {
+    public static void main(String[] args) {
+        //region Bài 01
+        /* Implement your code here */
+        // Khai báo các biến
+
+
+        //endregion
+
+        //region Bài 02
+        /* Implement your code here */
+
+
+        //endregion
+    }
+}
