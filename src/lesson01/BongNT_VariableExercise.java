445780698f7a300377fef67e66fbadba5dc95ef7
BongNT <liennguyen.5896@gmail.com>
2024-10-16 19:50:17 +0700

    Vincent: B├ái tß║¡p buß╗òi 02

--- src/lesson01/BongNT_VariableExercise.java
+++ src/lesson01/BongNT_VariableExercise.java
@@ -17,10 +17,46 @@ package lesson01;
 public class BongNT_VariableExercise {
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
-        //region Bài 01
+                //region Bài 01
         /* Implement your code here */
+        /*Khai báo các biến kiểu int, double, char, boolean, và String.*/
+        int myNum;
+        double myDoubleNum;
+        char myChar;
+        boolean myBoolean;
+        String myString;
 
+        /*Khởi tạo các biến với các giá trị cụ thể.*/
+        myNum = 10;
+        myDoubleNum = 16.10;
+        myChar = 'A';
+        myBoolean = true;
+        myString = "Testekk";
+
+        /*   Thực hiện các phép toán cộng, trừ, nhân, chia với các biến số nguyên và số thực.*/
+        int a = 10 + 5;
+        int b = 10 - 5;
+        int c = 10 * 5;
+        int d = 10 / 5;
+        double e = 10.01 + 5.01;
+        double f = 10.01 - 5.01;
+        double g = 10.01 * 5.01;
+        double h = 10.01 / 5.01;
+
+        /*In kết quả của các phép toán ra màn hình.*/
+        System.out.println("Phép toán cộng biến số nguyên: " + a);
+        System.out.println("Phép toán trừ biến số nguyên: " + b);
+        System.out.println("Phép toán nhân biến số nguyên: " + c);
+        System.out.println("Phép toán chia biến số nguyên: " + d);
+        System.out.println("Phép toán cộng biến số thực: " + e);
+        System.out.println("Phép toán trừ biến số thực: " + f);
+        System.out.println("Phép toán nhân biến số thực: " + g);
+        System.out.println("Phép toán chia biến số thực: " + h);
+
+        /*In giá trị của các biến kiểu char, boolean, và String.*/
+        System.out.println("In giá trị kiểu char: " + myChar);
+        System.out.println("In giá trị kiểu boolean: " + myBoolean);
+        System.out.println("In giá trị kiểu String: " + myString);
 
         //endregion
 
