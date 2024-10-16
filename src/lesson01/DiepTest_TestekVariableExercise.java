f621bbfab9f689e3ce1b8e73612bc7fd09a9847f
Adam Nguyen <adam.nguyen@ufinity.com>
2024-10-16 23:03:25 +0800

    Diep test 2

--- src/lesson01/DiepTest_TestekVariableExercise.java
+++ src/lesson01/DiepTest_TestekVariableExercise.java
@@ -3,6 +3,6 @@ package lesson01;
 public class TestekVariableExercise {
     public static void main(String[] args) {
         // Đây là file đáp án: Sẽ được gửi vào Thứ 5 (ngày 17/10/2024)
-
+        // test change
     }
 }
