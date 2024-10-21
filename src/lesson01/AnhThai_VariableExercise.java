11602b9a3c615f86a5b71328d2e8db8838816baa
anhthai07 <thaithoa307273@gmail.com>
2024-10-21 11:45:39 +0700

    AnhThai - Baitap Lesson01

---
+++ src/lesson01/AnhThai_VariableExercise.java
@@ -0,0 +1,71 @@
+package lesson01;//package lesson01;
+//
+///**
+// * Các bạn sẽ phát triển code của các bạn ở đây, lưu ý mỗi người tạo 01 file và đặt tên theo rule
+// * 1. Copy file này, và change name của file theo cú pháp sau: YourName_VariableExercise.java
+// *  Ví dụ: Nguyễn Văn An -> NguyenVanAn_VariableExercise.java
+// * 2. Truy cập file gitCommitMac.sh hoặc gitCommitWin.bat (Lựa chọn theo HĐH bạn đang sử dụng) và thêm thông tin commit của bạn.
+// *   2.1 Tại dòng 8: Thay đường dẫn file của bạn tại [Your file Path] (Ví dụ: src/lesson01/NguyenVanAn_VariableExercise.java)
+// *   2.2 Truy cập terminal có sử dụng IntelliJ IDEA: Vào View -> Tool Windows -> Terminal và thực thi dòng lệnh sau:
+// *    - Trên Mac: sh gitCommitMac.sh
+// *    - Trên Windows: gitCommitWin.bat
+// *    2.3 Nếu gặp lỗi về quyền truy cập thì bạn cần cấp quyền cho file .sh hoặc .bat đó, mở terminal và thực thi lệnh sau:
+// *    - Trên Mac: chmod +x gitCommitMac.sh
+// *    - Trên Windows: Không cần thực thi lệnh này
+// *  3. Sau khi thực thi xong, bạn sẽ thấy thông báo "Git commit successfully" trên terminal
+// */
+///*
+// * Review: Testek: Bạn đã hoàn thành bài tập rất tốt, code sạch, dễ đọc, đúng yêu cầu.
+// * Bài 01: Khai báo và khởi tạo các biến: chưa thực hiện đầy đủ yêu cầu
+// * Bài 02: Chưa thực hiện các phép toán logic với các biến boolean
+// * Bạn có thể tham khảo cách viết comment ở các bài tập khác để cải thiện comment của mình.
+// */
+//
+//public class AnhThai_VariableExercise {
+//    public static void main(String[] args) {
+//        //region Bài 01
+//        /* Implement your code here */
+//        int a = 25;
+//        int b = 75;
+//        double weight = 75.5;
+//        char grade = 'A';
+//        boolean isActive = true;
+//        String firstname = "Nguyen";
+//        String lastname = " Anh Thai";
+//
+//        int sum = a + b;
+//        System.out.println("Tong = "+sum);
+//
+//        int difference = b - a;
+//        System.out.println("Hieu = "+difference);
+//
+//        int product = a * b;
+//        System.out.println("a * b = "+ product);
+//
+//        double quotient = (double) b / a;
+//        System.out.println("b / a = "+ quotient);
+//
+//        //endregion
+//
+//        //region Bài 02
+//        /* Implement your code here */
+//        int x = 10;
+//        int y = 15;
+//        double z = 25.5;
+//        double w = 25.5;
+//        boolean isEqual;
+//        boolean isGreater;
+//
+//        isEqual = (y < x);
+//        System.out.println("isEqual = "+ isEqual);
+//
+//        isGreater = (z == w);
+//        System.out.println("isGreater = "+ isGreater);
+//
+//        String first = "Ten cua toi la";
+//        String last = " Nguyen Anh Thai";
+//        System.out.println(first + last + " va can nang = " +weight);
+//
+//        //endregion
+//    }
+//}
