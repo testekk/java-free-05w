1ec0e6c68d0008b0afb223bb6877c5f3fc275ca2
Lê Thị Hồng Diệp <lethihongdiep11395@gmail.com>
2024-10-24 12:33:31 +0700

    Dieplth: Bài tập buổi 3

--- src/lesson03/Dieplth_ConditionExercise.java
+++ src/lesson03/Dieplth_ConditionExercise.java
@@ -188,6 +188,16 @@ public class Dieplth_ConditionExercise {
         double c = scanner.nextDouble();
         /* Implement your code here */
         if ((a+b>c) && (a+c>b) && (b+c > a)){
+            if (a==b && b==c) {
+                System.out.println("Tam giác đều");
+            } else if (a==b || b==c || a==c) {
+                System.out.println("Tam giác cân");
+            } else if ((a*a + b*b == c*c) || (a*a + c*c == b*b) || (b*b + c*c == a*a)) {
+                System.out.println("Tam giác vuông");
+            } else {
+                System.out.println("Tam giác thường");
+            }
+
             System.out.println("Ba cạnh là ba cạnh của tam giác");
         } else {
             System.out.println("Không phải tam giác");
